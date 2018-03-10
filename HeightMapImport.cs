1939a52d9a277cd4346f9f1774179f5b02aecde7
Zigglegarf <zhembree654@gmail.com>
2018-03-10 12:49:26 -0500

    Changed heightMap param type to interface

--- HeightMapImport.cs
+++ HeightMapImport.cs
@@ -5,7 +5,7 @@ namespace HelmetVolumes
 {
     static partial class CMS
     {
-        public static Mesh ContourMesh(float[][][] heightMap, Vector3 _scale)
+        public static Mesh ContourMesh(IList<float>[][] heightMap, Vector3 _scale)
         {
             scale = _scale;
             step = new Vector3(1f / scale.x, 1f / scale.y, 1f / scale.z);
@@ -25,7 +25,7 @@ namespace HelmetVolumes
             return mesh;
         }
 
-        private static Octant[][][] GetOctants(float[][][] heightMap)
+        private static Octant[][][] GetOctants(IList<float>[][] heightMap)
         {
             int length = heightMap.Length, width = heightMap[0].Length;
             Octant[][][] octants = new Octant[length + 2][][];
@@ -49,9 +49,9 @@ namespace HelmetVolumes
                 for (int y = 0; y < width; y++)
                     if (heightMap[x][y] != null)
                     {
-                        octants[x + 1][y + 1] = new Octant[heightMap[x][y].Length];
+                        octants[x + 1][y + 1] = new Octant[heightMap[x][y].Count];
 
-                        for (int z = 0; z < heightMap[x][y].Length; z++)
+                        for (int z = 0; z < heightMap[x][y].Count; z++)
                             octants[x + 1][y + 1][z] = new Octant(x, y, heightMap[x][y][z]);
                     }
                     else
