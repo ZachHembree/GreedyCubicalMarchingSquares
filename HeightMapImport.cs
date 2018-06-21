a829011dc4216ee515e32850ec0515cb733f7618
Zach Hembree <zhembree654@gmail.com>
2018-06-21 07:01:57 -0400

    Revert "Mesh simplification bug fixes, new mesh importer, general refactoring."
    This reverts commit 0c52cd0ff74e87de91163ae095bf7e0fbfb3a35f.

--- HeightMapImport.cs
+++ HeightMapImport.cs
@@ -1,105 +1,61 @@
 ﻿using System.Collections.Generic;
-using System;
 using UnityEngine;
 
-namespace CmsMain
+namespace HelmetVolumes
 {
-    public partial class Volume
+    static partial class CMS
     {
-        public MeshData VoxelizeHeightmap(IList<float>[][] heightMap, Vector3 _scale, bool reduce = false)
+        public static Mesh ContourMesh(IList<float>[][] heightMap, Vector3 _scale)
         {
-            System.Diagnostics.Stopwatch importTimer = new System.Diagnostics.Stopwatch();
-            importTimer.Start();
-
-            if (_scale.x <= 0 || _scale.y <= 0 || _scale.z <= 0)
-                scale = Vector3.one;
-            else
-                scale = _scale;
-
+            scale = _scale;
             step = new Vector3(1f / scale.x, 1f / scale.y, 1f / scale.z);
             delta = new Vector3(scale.x / 2f, scale.y / 2f, scale.z / 2f);
-            dimensions = new Vector3Int(heightMap.Length + 2, heightMap[0].Length + 2, -1);
-            List<Octant>[][] octants = GetOctants(heightMap);
 
-            importTimer.Stop();
-            lastImportTime = importTimer.ElapsedMilliseconds;
+            List<Vector3> vertices;
+            List<Segment>[][][] segments;
+            GetSegments(GetOctants(heightMap), out vertices, out segments);
 
-            return ContourMesh(octants, reduce);
+            List<Square>[][][] squares = GetSquares(segments);
+            List<int> triangles = GetCubes(vertices, squares);
+
+            Mesh mesh = new Mesh();
+            mesh.vertices = vertices.ToArray();
+            mesh.triangles = triangles.ToArray();
+
+            return mesh;
         }
 
-        public List<Octant>[][] GetHeightmapOctants(IList<float>[][] heightMap, Vector3 _scale)
+        private static Octant[][][] GetOctants(IList<float>[][] heightMap)
         {
-            System.Diagnostics.Stopwatch importTimer = new System.Diagnostics.Stopwatch();
-            importTimer.Start();
-
-            if (_scale.x <= 0 || _scale.y <= 0 || _scale.z <= 0)
-                scale = Vector3.one;
-            else
-                scale = _scale;
-
-            step = new Vector3(1f / scale.x, 1f / scale.y, 1f / scale.z);
-            delta = new Vector3(scale.x / 2f, scale.y / 2f, scale.z / 2f);
-            dimensions = new Vector3Int(heightMap.Length + 2, heightMap[0].Length + 2, -1);
-            List<Octant>[][] octants = GetOctants(heightMap);
-
-            importTimer.Stop();
-            lastImportTime = importTimer.ElapsedMilliseconds;
-
-            return octants;
-        }
-
-        private List<Octant>[][] GetOctants(IList<float>[][] heightMap)
-        {
-            int length = heightMap.Length, width = heightMap[0].Length, next, n;
-            Octant last;
-            List<Octant>[][] octants = new List<Octant>[length + 2][];
+            int length = heightMap.Length, width = heightMap[0].Length;
+            Octant[][][] octants = new Octant[length + 2][][];
 
             for (int x = 0; x < octants.Length; x++)
-                octants[x] = new List<Octant>[width + 2];
+                octants[x] = new Octant[width + 2][];
 
             for (int x = 0; x < octants.Length; x++)
             {
-                octants[x][0] = new List<Octant>(0);
-                octants[x][width + 1] = new List<Octant>(0);
+                octants[x][0] = new Octant[0];
+                octants[x][width + 1] = new Octant[0];
             }
 
             for (int y = 1; y < octants[0].Length - 1; y++)
             {
-                octants[0][y] = new List<Octant>(0);
-                octants[length + 1][y] = new List<Octant>(0);
+                octants[0][y] = new Octant[0];
+                octants[length + 1][y] = new Octant[0];
             }
 
             for (int x = 0; x < length; x++)
                 for (int y = 0; y < width; y++)
                     if (heightMap[x][y] != null)
                     {
-                        last = Octant.zero; n = 1;
-                        octants[x + 1][y + 1] = new List<Octant>(heightMap[x][y].Count);
+                        octants[x + 1][y + 1] = new Octant[heightMap[x][y].Count];
 
                         for (int z = 0; z < heightMap[x][y].Count; z++)
-                        {
-                            next = (int)(heightMap[x][y][z] * step.z);
-
-                            if (next > last.range)
-                            {
-                                if (n > 1)
-                                {
-                                    last.z /= n;
-                                    n = 1;
-                                }
-
-                                last = new Octant(x * scale.x, y * scale.y, heightMap[x][y][z], next);
-                                octants[x + 1][y + 1].Add(last);
-                            }
-                            else
-                            {
-                                last.z += heightMap[x][y][z];
-                                n++;
-                            }
-                        }
+                            octants[x + 1][y + 1][z] = new Octant(x, y, heightMap[x][y][z]);
                     }
                     else
-                        octants[x + 1][y + 1] = new List<Octant>(0);
+                        octants[x + 1][y + 1] = new Octant[0];
 
             return octants;
         }
---
+++ HeightMapMono.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEditor;
+using HelmetVolumes;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+public class HeightMapMono : MonoBehaviour
+{
+    public Vector3 octantSize;
+    public float[][][] map;
+	
+    private MeshFilter meshFilter;
+    public string display { get; private set; }
+
+    public void GetMesh()
+    {
+        if (map != null)
+        {
+            Stopwatch timer = new Stopwatch();
+            meshFilter = GetComponent<MeshFilter>();
+
+            timer.Start();
+            meshFilter.sharedMesh = CMS.ContourMesh(map, octantSize);
+            timer.Stop();
+
+            meshFilter.sharedMesh.RecalculateNormals();
+
+            display =
+            (
+                "Total Time: " + timer.ElapsedMilliseconds + "ms\n\n" +
+                CMS.startVertices +
+                "Verticies: " + meshFilter.sharedMesh.vertexCount + "\n" +
+                "Triangles: " + (meshFilter.sharedMesh.triangles.Length / 3) + "\n" +
+                "Triangles/ms: " + (meshFilter.sharedMesh.triangles.Length / 3) / timer.ElapsedMilliseconds + " tris/ms\n" +
+                "Vertices/ms: " + meshFilter.sharedMesh.vertexCount / timer.ElapsedMilliseconds + " verts/ms\n"
+            );
+        }
+    }
+}
+
+[CustomEditor(typeof(MapGen))]
+public class HeightMapMonoInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        HeightMapMono cGen = (HeightMapMono)target;
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Generate Mesh"))
+            cGen.GetMesh();
+
+        EditorGUILayout.TextArea(cGen.display, GUILayout.MaxHeight(100));
+    }
+}
